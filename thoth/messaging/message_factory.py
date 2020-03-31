d235d264a20e0e9453a4579da3dd7ee34a032005
Kevin Postlethwait <k.postlethwait24@gmail.com>
2020-03-31 11:30:49 -0400

    Create simple message factory

---
+++ thoth/messaging/message_factory.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# thoth-messaging
+# Copyright(C) 2020 Kevin Postlethwait
+#
+# This program is free software: you can redistribute it and / or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+"""This is Thoth Messaging module for HashMismatchMessage."""
+
+import faust
+from typing import Tuple, Any
+from .message_base import MessageBase
+
+def message_factory(
+    t_name: str,
+    message_contents: Tuple[str, type],
+    num_partitions: int = 1,
+    replication_factor: int = 1
+    ): 
+    class NewMessage(MessageBase):
+        """Class used for Package Release events on Kafka topic."""
+
+        topic_name = t_name
+
+        class MessageContents(faust.Record, serializer="json"):
+            """Class used to represent a contents of a missing-package message Kafka topic."""
+            pass
+
+        MessageContents.__annotations__ = message_contents
+
+        def __init__(self, num_partitions: int = 1, replication_factor: int = 1):
+            """Initialize hash-mismatch topic."""
+            super(HashMismatchMessage, self).__init__(
+                self.topic_name,
+                value_type=self.MessageContents,
+                num_partitions=num_partitions,
+                replication_factor=replication_factor,
+            )
+
+    return NewMessage
