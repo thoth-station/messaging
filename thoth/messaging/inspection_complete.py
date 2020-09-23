2b91bbab647244421fae3ed9628f3a010630e1a6
KPostOffice <kpostlet@redhat.com>
2020-09-23 09:20:42 -0400

    add message for completed inspections (#270)

---
+++ thoth/messaging/inspection_complete.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# thoth-messaging
+# Copyright(C) 2019, 2020 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and / or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+"""This is Thoth Messaging module for InspectionCompleteMessage."""
+
+from .message_base import MessageBase, BaseMessageContents
+
+
+class InspectionCompletedMessage(MessageBase):
+    """Class used to indicate when amun inspections complete."""
+
+    topic_name = "thoth.inspection-completed"
+    _message_version = 1  # update on schema change
+
+    class MessageContents(BaseMessageContents, serializer="json"):  # type: ignore
+        """Class used to represent contents of a inspection-completed message Kafka topic."""
+
+        inspection_id: str
+        amun_api_url: str
+        deployment_name: str
+
+    def __init__(
+        self,
+        num_partitions: int = 1,
+        replication_factor: int = 1,
+        client_id: str = "thoth-messaging",
+        ssl_auth: int = 1,
+        bootstrap_server: str = "localhost:9092",
+        topic_retention_time_second: int = 60 * 60 * 24 * 45,
+        protocol: str = "SSL",
+    ):
+        """Initialize package releases topic."""
+        super(InspectionCompletedMessage, self).__init__(
+            topic_name=self.topic_name,
+            value_type=self.MessageContents,
+            num_partitions=num_partitions,
+            client_id=client_id,
+            ssl_auth=ssl_auth,
+            bootstrap_server=bootstrap_server,
+            topic_retention_time_second=topic_retention_time_second,
+            protocol=protocol,
+            message_version=self._message_version,
+        )
